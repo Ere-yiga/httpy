ada62d4d4a4366d8a1786fe15434d5187a3988f4
Ere-yiga <erehdagr8@gmail.com>
2026-08-07 23:14:44 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+//	An HTTP server that'll give us hello world and a goodbye endpoint. We will also write test that covers both
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello world")
+}
