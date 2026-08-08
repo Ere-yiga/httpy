3a85455bbd7a86e3cfd5d3e67fbfc9b7c22bd1c9
Ere-yiga <erehdagr8@gmail.com>
2026-08-08 23:13:18 +0100

    a basic net/http server, JSON encode & decode handler

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
-//	An HTTP server that'll give us hello world and a goodbye endpoint. We will also write test that covers both
-
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Hello world")
+type Human struct {
+	Name string  `json:"name"`
+	Age  float64 `json:"age"`
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
+
+func humanHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Encoding
+	p := Human{Name: "Shalom", Age: 49}
+	json.NewEncoder(w).Encode(p)
+}
+
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+	
+	// Decoding
+	var p Human
+	json.NewDecoder(r.Body).Decode(&p)
+	fmt.Println("Received:", p.Name, p.Age)
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/human", humanHandler)
+	http.HandleFunc("/decode", decodeHandler)
+
+	fmt.Println("Listening on :8080...")
+	http.ListenAndServe(":8080", nil)
 }
